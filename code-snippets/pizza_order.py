d067b55b9d6014a5f2b2d01cb532f0c90809d954
Jay Wang, D.Sc <wcj365@gmail.com>
2020-06-13 22:23:03 -0400

    Create pizza_order.py

---
+++ code-snippets/pizza_order.py
@@ -0,0 +1,33 @@
+from faker import Faker
+
+Class PizzaOrder:
+
+    def __init__(self, first_name, last_name, order_amount, order_date):
+        self.first_name = first_name
+        self.last_name =last_name
+        self.order_amount = order_amount
+        self.order_date = order_date
+
+
+    def store_customer_Name(first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def retrieve_customer_name():
+        return (first_name, last_name)
+
+
+    def store_order_amount(order_amount):
+        self.order_amount = order_amount
+
+    def retrieve_order_amount():
+        return order_amount
+
+    def store_order_date(order_date):
+        self.order_date = order_date
+
+    def retrieve_order_date():
+        return order_date
+
+def order_robot():
+    
